d07520896fdd694fa58a7445565c70ec6e7b8435
JoinFyc <fyc139050@163.com>
2024-08-14 16:09:21 +0800

    Fix Bug --> compiler

--- base-code-generator/common/service/integration/src/main/java/com/fei/base/common/service/integration/config/interceptors/LogInterceptor.java
+++ base-code-generator/common/service/integration/src/main/java/com/fei/base/common/service/integration/config/interceptors/LogInterceptor.java
@@ -1,10 +1,10 @@
 package com.fei.base.common.service.integration.config.interceptors;
 
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.HandlerInterceptor;
 
 /**
  * @author fei
  * @date 2020-09-22 16:16
  **/
-public class LogInterceptor extends HandlerInterceptorAdapter {
+public class LogInterceptor implements HandlerInterceptor {
 }
